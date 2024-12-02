2e8cb926f31864fb72023e13781a4a6fcb874539
cpennington-de <65792142+cpennington-de@users.noreply.github.com>
2024-12-01 22:28:39 -0600

    changed indent of lint config file

--- eslint.config.js
+++ eslint.config.js
@@ -9,10 +9,10 @@ module.exports = [
         sourceType: 'module'
       },
       rules: {
-        'indent': ['error', 2],
+        indent: ['error', 2],
         'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always']
+        quotes: ['error', 'single'],
+        semi: ['error', 'always']
       }
     }
   ];
